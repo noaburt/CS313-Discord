0465b7c50c98e67ed70a1063ed383d525a7ca512
noaburt <noaburt051221@gmail.com>
2025-01-29 12:04:38 +0000

    THREADS

--- TestServer.java
+++ TestServer.java
@@ -1,15 +1,21 @@
+import com.sun.tools.javac.Main;
 
-public class TestServer {
+public class TestServer extends Thread {
 
     public static void main(String[] args) {
-        SimpleServerClient server = new SimpleServerClient();
-        server.start(6666);
+        TestServer thread = new TestServer();
+        thread.start();
 
         SimpleClient client = new SimpleClient();
-        client.startConnection("127.0.0.1", 6666);
+        client.startConnection("127.0.0.1", 4444);
         String response = client.sendMessage("hello server");
 
         System.out.println(response);
+    }
+
+    public void run() {
+        SimpleServerClient server = new SimpleServerClient();
+        server.start(4444);
     }
 
 }
