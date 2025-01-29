7fe62e029edff92224b5ee43c3b02f56a8cde4cf
noaburt <noaburt051221@gmail.com>
2025-01-29 11:21:16 +0000

    Added simple code for client

--- src/SimpleClient.java
+++ src/SimpleClient.java
@@ -1,1 +1,27 @@
 
+import java.net.*;
+import java.io.*;
+
+public class SimpleClient {
+    private Socket clientSocket;
+    private PrintWriter output;
+    private BufferedReader input;
+
+    public void makeConnection(String ip, int port) {
+        clientSocket = new Socket(ip, port);
+        output = new PrintWriter(clientSocket.getOutputStream(), true);
+        input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+
+    public String sendMessage(String message) {
+        output.println(message);
+        String resp = input.readLine();
+        return resp;
+    }
+
+    public void closeConnection() {
+        input.close();
+        output.close();
+        clientSocket.close();
+    }
+}
