f1ccb5b9b36ec22d5d182dae2fce24c5030add04
noaburt <noaburt051221@gmail/com>
2025-03-28 13:54:04 +0000

    Correct code now

--- src/NotSoSimple/NotEvenCloseToSimpleSelectionPage.java
+++ src/NotSoSimple/NotEvenCloseToSimpleSelectionPage.java
@@ -157,8 +157,9 @@ public class NotEvenCloseToSimpleSelectionPage extends form{
                          */
                         client.connected = false;
                         client.setRoomCode("");
+
                         /* Notify server that client has left */
-                        client.sendMessage("", commonconstants.reqCodes.LEAVE,"","");
+                        client.sendMessage("", commonconstants.reqCodes.DISCONNECT,"","");
                         new NotSoSimpleClientLoginPage(null,0).setVisible(true);
                         NotEvenCloseToSimpleSelectionPage.this.dispose();
                     }
